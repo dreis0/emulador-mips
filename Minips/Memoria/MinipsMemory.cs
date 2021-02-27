e058f093c2bfd583ecec30aec870ba5210e4e945
Miguel dos Reis <migueldreis0@gmail.com>
2021-02-27 16:27:54 -0300

    added: primeira versão da simulação de memória usando hash table

---
+++ Minips/Memoria/MinipsMemory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Minips.Memory
+{
+    public class MinipsMemory
+    {
+        /*
+            Os endereços são múltiplos de 4 0x00000000 -> 0x00000004 -> 0x00000008
+            Endereço inicial 0x00400000
+         
+         */
+        private readonly Hashtable _hashTable;
+
+        public MinipsMemory()
+        {
+            _hashTable = new Hashtable();
+        }
+
+        public void Write(int address, byte[] value)
+        {
+            if (value.Length != 4)
+                throw new InvalidOperationException("Uma palavra deve conter 32 bits");
+
+            _hashTable[address] = value;
+        }
+
+        public byte[] Read(int address)
+        {
+            var value = _hashTable[address];
+            return value as byte[];
+        }
+    }
+}
