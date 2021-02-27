ccccb88138bff976b083455d6538b2412cbf63ef
Miguel dos Reis <migueldreis0@gmail.com>
2021-02-27 16:28:20 -0300

    added: carregamento das entradas para a hash table

--- Minips/Program.cs
+++ Minips/Program.cs
@@ -1,12 +1,55 @@
-﻿using System;
+﻿using Minips.Instructions;
+using Minips.Memory;
+using System;
+using System.IO;
 
 namespace minips
 {
     class Program
     {
+        const int TEXT_SECTION_START = 0x00400000;
+        const int DATA_SECTION_START = 0x10010000;
+
+        private static MinipsMemory _memory;
+        private static MinipsMemory _registers;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            _memory = new MinipsMemory();
+            _registers = new MinipsMemory();
+
+            string textFile = "C:\\Users\\Miguel dos Reis\\OneDrive\\UFABC\\Arquitetura de Computadores\\Projeto\\01.soma.text";
+            string dataFile = "C:\\Users\\Miguel dos Reis\\OneDrive\\UFABC\\Arquitetura de Computadores\\Projeto\\01.soma.data";
+
+            // Loads .text
+            using (var binaryReader = new BinaryReader(File.Open(textFile, FileMode.Open, FileAccess.Read, FileShare.Read)))
+            {
+                byte[] bytes = binaryReader.ReadBytes(4);
+                int address = TEXT_SECTION_START;
+
+                while (bytes != null && bytes.Length == 4)
+                {
+                    _memory.Write(address, bytes);
+
+                    bytes = binaryReader.ReadBytes(4);
+                    address += 4;
+                }
+            }
+
+            // Loads .data
+            using (var binaryReader = new BinaryReader(File.Open(dataFile, FileMode.Open, FileAccess.Read, FileShare.Read)))
+            {
+                byte[] bytes = binaryReader.ReadBytes(4); ;
+                int address = DATA_SECTION_START;
+
+                while (bytes != null && bytes.Length == 4)
+                {
+                    _memory.Write(address, bytes);
+
+                    bytes = binaryReader.ReadBytes(4);
+                    address += 4;
+                } 
+            }
         }
     }
 }
