95425425552ee2155fe1aea2ddb8d0eefef1c3fc
Miguel dos Reis <migueldreis0@gmail.com>
2021-03-06 19:56:27 -0300

    added: classe para auxiliar na decodificação

---
+++ Minips/Instrucoes/InstructionInfo.cs
@@ -0,0 +1,15 @@
+﻿namespace Minips.Instructions
+{
+    public class InstructionInfo
+    {
+        public char Format { get; set; }
+
+        public int OpCode { get; set; }
+
+        public int? Funct { get; set; }
+
+        public string Mnemonic { get; set; }
+
+        public InstructionType Type { get; set; }
+    }
+}
